3893a69ded6941e2762345e72375b56e34ac6cc6
Will Wang <mindnlight@gmail.com>
2013-06-19 04:07:39 -0400

    Took out Username fields

--- LossBoysCustomerAppProject/LossBoysCustomerApp/build/source/r/debug/com/lossboys/customerapp/R.java
+++ LossBoysCustomerAppProject/LossBoysCustomerApp/build/source/r/debug/com/lossboys/customerapp/R.java
@@ -18,25 +18,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
-        public static final int header=0x7f080002;
-        public static final int loginButton=0x7f080000;
-        public static final int loginLink=0x7f08000a;
-        public static final int loginRegister=0x7f080009;
-        public static final int registerButton=0x7f080001;
-        public static final int registerEmail=0x7f080006;
-        public static final int registerFirst=0x7f080004;
-        public static final int registerLast=0x7f080005;
-        public static final int registerLink=0x7f080003;
-        public static final int registerPassword=0x7f080008;
-        public static final int registerUsername=0x7f080007;
+        public static final int action_settings=0x7f080009;
+        public static final int header=0x7f080000;
+        public static final int loginButton=0x7f080001;
+        public static final int loginLink=0x7f080008;
+        public static final int loginRegister=0x7f080007;
+        public static final int registerEmail=0x7f080005;
+        public static final int registerFirst=0x7f080003;
+        public static final int registerLast=0x7f080004;
+        public static final int registerLink=0x7f080002;
+        public static final int registerPassword=0x7f080006;
     }
     public static final class layout {
         public static final int footer=0x7f030000;
         public static final int header=0x7f030001;
-        public static final int home=0x7f030002;
-        public static final int login=0x7f030003;
-        public static final int register=0x7f030004;
+        public static final int login=0x7f030002;
+        public static final int register=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
