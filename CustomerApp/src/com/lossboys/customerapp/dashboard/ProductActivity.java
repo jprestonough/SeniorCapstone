45e61c45325df670fa7757f46a10138339591e1a
J Preston Ough <jough@gatech.edu>
2013-06-21 21:44:19 -0400

    Added product page and connected to database

---
+++ CustomerApp/src/com/lossboys/customerapp/dashboard/ProductActivity.java
@@ -0,0 +1,48 @@
+package com.lossboys.customerapp.dashboard;
+
+import java.net.URL;
+
+import com.lossboys.customerapp.R;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ProductActivity extends Activity {
+	TextView itemName,itemDepartment,itemDescription,itemPrice,itemQuantity;
+	ImageView itemImage;
+	
+	 /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_layout);
+        
+        itemName = (TextView) findViewById(R.id.item_name);
+        itemDepartment = (TextView) findViewById(R.id.item_department);
+        itemDescription = (TextView) findViewById(R.id.item_description);
+        itemPrice = (TextView) findViewById(R.id.item_price);
+        itemQuantity = (TextView) findViewById(R.id.item_quantity);
+        itemImage = (ImageView) findViewById(R.id.item_image);
+        
+        itemName.setText(getIntent().getStringExtra("Name"));
+        itemDepartment.setText(getIntent().getStringExtra("Department"));
+        itemDescription.setText(getIntent().getStringExtra("Description"));
+        itemPrice.setText("$"+getIntent().getStringExtra("Price"));
+        itemQuantity.setText(getIntent().getStringExtra("Quantity"));
+        
+        URL url;
+		try {
+			url = new URL("http://23.21.158.161:4912/item_images/"+getIntent().getStringExtra("ItemID")+".jpg");
+			Log.d("product","http://23.21.158.161:4912/item_images/"+getIntent().getStringExtra("ItemID")+".jpg");
+			Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+			itemImage.setImageBitmap(bmp);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        
+    }
+}
---
+++ CustomerApp/src/com/lossboys/customerapp/dashboard/ScanActivity.java
@@ -0,0 +1,74 @@
+package com.lossboys.customerapp.dashboard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import com.lossboys.customerapp.CustomHTTP;
+import com.lossboys.customerapp.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ScanActivity extends Activity {
+	Button btnItemID;
+    EditText inputItemID;
+    TextView scanErrorMsg;
+    
+	 /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scan_layout);
+        
+		// Importing all assets like buttons, text fields
+        inputItemID = (EditText) findViewById(R.id.ItemIdField);
+        btnItemID = (Button) findViewById(R.id.btnItemID);
+        scanErrorMsg = (TextView) findViewById(R.id.scan_error);
+                
+        btnItemID.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				String ItemID = inputItemID.getText().toString();
+
+                // Building post parameters
+                // key and value pair
+                List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(1);
+                nameValuePair.add(new BasicNameValuePair("ItemID", ItemID));
+                
+                JSONObject scanJSON = CustomHTTP.makePOST("http://23.21.158.161:4912/get_item.php", nameValuePair);
+                
+                if(scanJSON != null){
+    				try {
+    					if(!scanJSON.isNull("Error")){
+    						scanErrorMsg.setText("Item does not exist.");
+    					}else{
+    						Intent i = new Intent(getApplicationContext(),
+    				                com.lossboys.customerapp.dashboard.ProductActivity.class);
+
+    						i.putExtra("Name",scanJSON.getString("Name"));
+    						i.putExtra("Department",scanJSON.getString("Department"));
+    						i.putExtra("Description",scanJSON.getString("Description"));
+    						i.putExtra("Price",scanJSON.getString("Price"));
+    						i.putExtra("Quantity",scanJSON.getString("Quantity"));
+    						i.putExtra("ItemID",ItemID);
+    						
+        	                startActivity(i);
+    					}
+    				} catch (Exception e) {
+    					e.printStackTrace();
+    					scanErrorMsg.setText("Lookup failed.");
+    				}
+                } else
+                	scanErrorMsg.setText("Lookup failed.");
+			}
+		});
+    }
+}
