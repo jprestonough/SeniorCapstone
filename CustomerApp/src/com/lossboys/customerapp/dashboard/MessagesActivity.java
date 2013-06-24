1fb54184ac5c922276f034d1a40cf05932070bf2
Will Wang <mindnlight@gmail.com>
2013-06-24 17:07:42 -0400

    Cleaned up the layout of ProductInfo and banner sizes

---
+++ CustomerApp/src/com/lossboys/customerapp/dashboard/MessagesActivity.java
@@ -0,0 +1,16 @@
+package com.lossboys.customerapp.dashboard;
+
+import com.lossboys.customerapp.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class MessagesActivity extends Activity {
+	 /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.messages_layout);
+    }
+}
