c931265da91fa5de612702a1b5bb9cac7760215f
J Preston Ough <jough@gatech.edu>
2013-07-21 18:43:20 -0400

    finished inventory and added order email

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -1,7 +1,7 @@
 <?php
 // For debug
-ini_set('display_errors', '1');
-error_reporting(E_ALL);
+//ini_set('display_errors', '1');
+//error_reporting(E_ALL);
 require_once("includes/vars.php");
 require_once("logic/session_control.php");
 require_once("logic/login_control.php");
--- public_html/inventory.php
+++ public_html/inventory.php
@@ -1,6 +1,17 @@
 <?PHP
 include("includes/header.php");
 
+function getEmail($dbh,$oid){
+    $stmt = $dbh->prepare("select CustomerEmail from Customer join CustomerOrder on Customer_CustomerID = CustomerID
+		where OrderID = ?;");
+    $stmt->bindParam(1, $oid);
+    $stmt->execute();
+
+    $data = $stmt->fetchAll();
+
+    return $data[0][0];
+}
+
 if(!empty($_POST['function'])){
 	switch ($_POST['function']) {
 		case 'check_orders':
@@ -23,8 +34,7 @@ if(!empty($_POST['function'])){
                         $stmt = $dbh->prepare("select Name,OrderItem.Quantity,Price,ItemID from OrderItem
                                 join Item on Item_ItemID = ItemID
                                 where CustomerOrder_OrderID = ?
-				and OrderItem.ItemStatus_ItemStatusID = 1
-				and OrderItem.Quantity <> 0");
+				and OrderItem.ItemStatus_ItemStatusID = 1 and OrderItem.Quantity <> 0");
                     $stmt->bindParam(1, $_POST['orderid']);
                     $stmt->execute();
 
@@ -36,14 +46,42 @@ if(!empty($_POST['function'])){
 
 			$stmt = $dbh->prepare("select Quantity from OrderItem
 				where CustomerOrder_OrderID = ? and Item_ItemID = ?
+				and ItemStatus_ItemStatusID = 1");
+			    $stmt->bindParam(1, $_POST['orderid']);
+			    $stmt->bindParam(2, $_POST['itemid']);
+			    $stmt->execute();
+
+			    $data = $stmt->fetchAll();
+
+			if(count($data) == 0){
+				echo '{"error":"invalid"}';
+				return;
+			}
+
+			$stmt = $dbh->prepare("select Quantity from OrderItem
+				where CustomerOrder_OrderID = ? and Item_ItemID = ?
+				and ItemStatus_ItemStatusID = 1");
+			    $stmt->bindParam(1, $_POST['orderid']);
+			    $stmt->bindParam(2, $_POST['itemid']);
+			    $stmt->execute();
+
+			    $data = $stmt->fetchAll();
+
+			if($data[0][0] == '0'){
+				echo '{"error":"done"}';
+				return;
+			}
+
+			$stmt = $dbh->prepare("select Quantity from OrderItem
+				where CustomerOrder_OrderID = ? and Item_ItemID = ?
 				and ItemStatus_ItemStatusID = 3");
-		    $stmt->bindParam(1, $_POST['orderid']);
-		    $stmt->bindParam(2, $_POST['itemid']);
-		    $stmt->execute();
+			    $stmt->bindParam(1, $_POST['orderid']);
+			    $stmt->bindParam(2, $_POST['itemid']);
+			    $stmt->execute();
 
-		    $data = $stmt->fetchAll();
+			    $data = $stmt->fetchAll();
 
-	        if(count($data) == 0){
+			if(count($data) == 0){
 				$stmt = $dbh->prepare("insert into OrderItem (Item_ItemID,CustomerOrder_OrderID,ItemStatus_ItemStatusID,Quantity)
 					values (?,?,3,1);");
 				$stmt->bindParam(1, $_POST['itemid']);
@@ -55,17 +93,34 @@ if(!empty($_POST['function'])){
 			        $stmt->bindParam(1, $_POST['orderid']);
 			        $stmt->bindParam(2, $_POST['itemid']);
 			}
+
 		    if(!$stmt->execute())
 		    	echo '{"error":"true"}';
+
 		   $stmt = $dbh->prepare("update OrderItem set Quantity = Quantity - 1
 			where CustomerOrder_OrderID = ? and Item_ItemID = ?
 			and ItemStatus_ItemStatusID = 1");
 		   $stmt->bindParam(1, $_POST['orderid']);
 		   $stmt->bindParam(2, $_POST['itemid']);
+
 		    if($stmt->execute())
 		    	echo '{"error":"false"}';
-		    else
-		    	echo '{"error":"true"}';
+
+			$stmt = $dbh->prepare("select Quantity from OrderItem
+				where CustomerOrder_OrderID = ? 
+				and ItemStatus_ItemStatusID = 1 and Quantity <> 0");
+			    $stmt->bindParam(1, $_POST['orderid']);
+			    $stmt->execute();
+
+			    $data = $stmt->fetchAll();
+
+			if(count($data) == 0){
+				echo '{"error":"orderdone"}';
+			   $message = "Your order has been completed. Please pick it up at the checkout counter.";
+
+			   smtpmailer(getEmail($dbh,$_POST['orderid']), 'thelossboys@gmail.com', 'Loss', 'Order Completed', $message);
+			}
+
 		break;
 		default:
 			# code...
