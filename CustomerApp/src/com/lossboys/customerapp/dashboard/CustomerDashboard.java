ca4ff3c3868253d739831265f9061d8d496e06d6
Nianzu Wang <nwang6@mail.gatech.edu>
2013-07-21 18:03:36 -0400

    Finalized some UI aspects of the Customer App. Should be ready for final presentation

--- CustomerApp/src/com/lossboys/customerapp/dashboard/CustomerDashboard.java
+++ CustomerApp/src/com/lossboys/customerapp/dashboard/CustomerDashboard.java
@@ -69,40 +69,28 @@ public class CustomerDashboard extends Activity {
 		/**
 		 * Creating all buttons instances
 		 * */
-		// Dashboard News feed button
-		Button btn_logout = (Button) findViewById(R.id.btn_logout);
 
-		// Dashboard Friends button
+		// Dashboard Scan button
 		Button btn_scan = (Button) findViewById(R.id.btn_scan);
 
-		// Dashboard Messages button
+		// Dashboard Cart button
 		Button btn_cart = (Button) findViewById(R.id.btn_cart);
 
-		// Dashboard Checkout button
-		Button btn_checkout = (Button) findViewById(R.id.btn_checkout);
-
-		// Dashboard Events button
-		Button btn_account = (Button) findViewById(R.id.btn_account);
-
-		// Dashboard Photos button
-		Button btn_photos = (Button) findViewById(R.id.btn_photos);
-
-		btn_photos.setVisibility(View.INVISIBLE);
 
 		/**
 		 * Handling all button click events
 		 * */
 
-		btn_logout.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(0);
-				CustomHTTP.makePOST("http://23.21.158.161:4912/logout.php", nameValuePair);
-				Intent i = new Intent(getApplicationContext(), com.lossboys.customerapp.CustomerLogin.class);
-				startActivity(i);
-				finish();
-			}
-		});
+//		btn_logout.setOnClickListener(new View.OnClickListener() {
+//			@Override
+//			public void onClick(View view) {
+//				List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(0);
+//				CustomHTTP.makePOST("http://23.21.158.161:4912/logout.php", nameValuePair);
+//				Intent i = new Intent(getApplicationContext(), com.lossboys.customerapp.CustomerLogin.class);
+//				startActivity(i);
+//				finish();
+//			}
+//		});
 
 		// Listening Friends button click
 		btn_scan.setOnClickListener(new View.OnClickListener() {
@@ -128,37 +116,27 @@ public class CustomerDashboard extends Activity {
 
 		
 		// Listening to Places button click
-		btn_checkout.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View view) {
-				// Launching News Feed Screen
-				Intent i = new Intent(getApplicationContext(), CheckoutActivity.class);
-				startActivity(i);
-			}
-		});
+//		btn_checkout.setOnClickListener(new View.OnClickListener() {
+//
+//			@Override
+//			public void onClick(View view) {
+//				// Launching News Feed Screen
+//				Intent i = new Intent(getApplicationContext(), CheckoutActivity.class);
+//				startActivity(i);
+//			}
+//		});
 		
 		
 		// Listening to Events button click
-		btn_account.setOnClickListener(new View.OnClickListener() {
+//		btn_account.setOnClickListener(new View.OnClickListener() {
+//
+//			@Override
+//			public void onClick(View view) {
+//				// Launching News Feed Screen
+//				Intent i = new Intent(getApplicationContext(), AccountActivity.class);
+//				startActivity(i);
+//			}
+//		});
 
-			@Override
-			public void onClick(View view) {
-				// Launching News Feed Screen
-				Intent i = new Intent(getApplicationContext(), AccountActivity.class);
-				startActivity(i);
-			}
-		});
-
-		// Listening to Photos button click
-		btn_photos.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View view) {
-				// Launching News Feed Screen
-				Intent i = new Intent(getApplicationContext(), PhotosActivity.class);
-				startActivity(i);
-			}
-		});
 	}
 }
